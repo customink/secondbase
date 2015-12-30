5f1f6e4fc23794b75e11c73695421a7b8e115b30
Ken Collins <ken@metaskills.net>
2015-12-30 07:37:03 -0500

    Add Dummy::Application for testing.

---
+++ test/dummy_app/init.rb
@@ -0,0 +1,35 @@
+ENV['RAILS_ENV'] ||= 'test'
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../../Gemfile', __FILE__)
+require 'bundler/setup'
+require 'rails/all'
+Bundler.require(:default, Rails.env)
+
+module Dummy
+  class Application < ::Rails::Application
+
+    # Basic Engine
+    config.root = File.join __FILE__, '..'
+    config.cache_store = :memory_store
+    config.assets.enabled = false
+    config.secret_token = '012345678901234567890123456789'
+    config.active_support.test_order = :random
+
+    # Mimic Test Environment Config.
+    config.consider_all_requests_local = true
+    config.action_controller.perform_caching = false
+    config.action_dispatch.show_exceptions = false
+    config.action_controller.allow_forgery_protection = false
+    config.action_mailer.delivery_method = :test
+    config.active_support.deprecation = :stderr
+    config.allow_concurrency = true
+    config.cache_classes = true
+    config.dependency_loading = true
+    config.preload_frameworks = true
+    config.eager_load = true
+    config.secret_key_base = '012345678901234567890123456789'
+
+  end
+end
+
+Dummy::Application.initialize!
+require 'rails/test_help'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ require 'secondbase'
 # require 'secondbase/model'
 require 'active_support/test_case'
 require 'active_support/testing/autorun'
+require 'dummy_app/init'
 
 ActiveSupport.test_order = :random if ActiveSupport.respond_to?(:test_order)
 
