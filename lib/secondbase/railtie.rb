16104c52f2984a58756eb87ef1343e5526aa9e56
Ken Collins <ken@metaskills.net>
2015-12-30 15:40:22 -0500

    Create app path for db/secondbase. Make dir if none exists.

--- lib/secondbase/railtie.rb
+++ lib/secondbase/railtie.rb
@@ -1,6 +1,16 @@
 module SecondBase
   class Railtie < Rails::Railtie
 
+    config.secondbase = ActiveSupport::OrderedOptions.new
+    config.secondbase.path = 'db/secondbase'
+
+    config.after_initialize do |app|
+      path = config.secondbase.path
+      pdir = app.root.join(path)
+      FileUtils.mkdir(pdir) unless File.directory?(pdir)
+      app.paths.add(path)
+    end
+
     rake_tasks do
       load 'secondbase/databases.rake'
     end
