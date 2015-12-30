a61f904319b223e3fb0fc0945b7c33fbe7b6d512
Ken Collins <ken@metaskills.net>
2015-12-29 20:07:14 -0500

    Let's assume we do not need the `reset_visitor_cache` in a new architecture.

--- lib/secondbase.rb
+++ lib/secondbase.rb
@@ -12,22 +12,12 @@ module SecondBase
 
   def self.has_runner(env)
     ActiveRecord::Base.establish_connection(SecondBase::config(env))
-    reset_visitor_cache
   end
 
   def self.config(env)
     ActiveRecord::Base.configurations[SecondBase::CONNECTION_PREFIX][env]
   end
 
-  # TODO: We should really look at faking out the connection used by ActiveRecord
-  # during migrations, this would prevent us from digging around Arel internals.
-  # Arel caches the SQL translator based on the engine (ActiveRecord::Base).  This
-  # means that if we swap out the base connection we risk the SQL translator being wrong.
-  # This is an ugly hack that resets the adapter.  See Line 27 of Arel's visitors.rb class.
-  def self.reset_visitor_cache
-    engine = ActiveRecord::Base
-    Arel::Visitors::ENGINE_VISITORS[engine] = engine.connection.visitor
-  end
 end
 
 module FirstBase
@@ -37,6 +27,5 @@ module FirstBase
 
   def self.has_runner(env)
     ActiveRecord::Base.establish_connection(FirstBase::config(env))
-    SecondBase.reset_visitor_cache
   end
 end
