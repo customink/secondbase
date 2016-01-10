d2a1cda1a29d196c1426da872a9d7d43777e55e3
Ken Collins <ken@metaskills.net>
2016-01-10 13:46:02 -0500

    Like ActiveRecord, add SecondBase schema/structure to watchable_files.

--- lib/second_base/railtie.rb
+++ lib/second_base/railtie.rb
@@ -18,6 +18,11 @@ module SecondBase
       require 'rails/second_base/generators/migration_generator'
     end
 
+    initializer 'second_base.add_watchable_files' do |app|
+      secondbase_dir = app.root.join(config.second_base.path)
+      config.watchable_files.concat ["#{secondbase_dir}/schema.rb", "#{secondbase_dir}/structure.sql"]
+    end
+
     def config_path
       config.second_base.path
     end
