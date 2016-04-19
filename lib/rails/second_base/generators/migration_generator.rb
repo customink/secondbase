61cb1bc111e4657c83c5447a1981292431157009
Ken Collins <ken@metaskills.net>
2016-04-19 00:22:34 -0400

    Test base migrator fix.

--- lib/rails/second_base/generators/migration_generator.rb
+++ lib/rails/second_base/generators/migration_generator.rb
@@ -8,6 +8,7 @@ module SecondBase
     source_root ActiveRecord::Generators::MigrationGenerator.source_root
 
     def self.desc
+      require 'rails/generators/rails/migration/migration_generator'
       Rails::Generators::MigrationGenerator.desc
     end
 
